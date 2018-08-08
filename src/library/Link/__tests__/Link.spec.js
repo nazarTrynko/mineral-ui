425c3f52fd4fd7fc337e061607d8f62cb14c7696
Brent Ertz <brent.ertz@ca.com>
2018-08-08 14:04:59 -0600

    WIP: Add Link root node specs

--- src/library/Link/__tests__/Link.spec.js
+++ src/library/Link/__tests__/Link.spec.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Link, { componentTheme } from '../Link';
 import examples from '../../../website/app/demos/Link/examples';
+import { mountInWrapper } from '../../../../utils/enzymeUtils';
 import testDemoExamples from '../../../../utils/testDemoExamples';
 import testThemeOverrides from '../../../../utils/testThemeOverrides';
 import { getProcessedComponentThemeKeys } from '../../themes/processComponentTheme';
@@ -30,5 +31,33 @@ describe('Link', () => {
       <Link href="http://example.com">test</Link>,
       getProcessedComponentThemeKeys(componentTheme)
     );
+  });
+
+  describe('root node', () => {
+    let wrapper;
+
+    beforeEach(() => {
+      Link.createRootNode = jest
+        .fn()
+        .mockImplementation((props) => props.element);
+      wrapper = mountInWrapper(<Link href="http://example.com">test</Link>);
+    });
+
+    afterEach(() => {
+      Link.createRootNode.mockReset();
+    });
+
+    it('is updated when element prop changes', () => {
+      wrapper.setProps({ element: 'span' });
+
+      expect(Link.createRootNode).toHaveBeenCalledTimes(2);
+    });
+
+    it('is not updated when other props change', () => {
+      wrapper.setProps({ id: 'test' });
+      wrapper.setProps({ onClick: jest.fn() });
+
+      expect(Link.createRootNode).toHaveBeenCalledTimes(1);
+    });
   });
 });
